46a0ad71b41afb6bba8cefa27fc49284ba596ab9
Sebastian Tellez <sebastian.tellez.o@gmail.com>
2021-02-20 14:07:58 -0600

    add second layout constraintset

--- androidlayouts/app/src/main/java/com/mobilestudio/androidlayouts/MainActivity.java
+++ androidlayouts/app/src/main/java/com/mobilestudio/androidlayouts/MainActivity.java
@@ -1,14 +1,25 @@
 package com.mobilestudio.androidlayouts;
 
 import android.os.Bundle;
-
+import android.widget.ImageView;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintSet;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView imgLog;
+
+    private ConstraintSet contraintStart = new ConstraintSet();
+    private ConstraintSet contraintEnd = new ConstraintSet();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_start);
+
+        imgLog = findViewById(R.id.imgLogoLogin);
+
+        imgLog.setOnClickListener(v -> { });
+
     }
 }
